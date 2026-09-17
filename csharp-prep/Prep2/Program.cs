0539452191b30546fcd7bd3ea69034d48bd8584c
Bothstingray350 <bubberjr1@gmail.com>
2026-09-17 16:50:26 -0600

    Prep1 and Prep2 programs completed

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,39 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade? (0-100): ");
+        string strGrade = Console.ReadLine();
+        float intGrade = float.Parse(strGrade);
+        char letterGrade = 'X';
+        switch (intGrade) {
+            case float number when number >= 90:
+                letterGrade = 'A';
+                break;
+            case float number when number >= 80:
+                letterGrade = 'B';
+                break;
+            case float number when number >= 70:
+                letterGrade = 'C';
+                break;
+            case float number when number >= 60:
+                letterGrade = 'D';
+                break;
+            case float number when number < 60:
+                letterGrade = 'F';
+                break;
+        }
+
+        Console.WriteLine($"Your letter grade is {letterGrade}");
+
+        if (intGrade >= 70)
+        {
+            Console.WriteLine("You've passed, good job!");
+        }
+        else
+        {
+            Console.WriteLine("You've failed, better luck next time");
+        }
+
+
     }
 }
